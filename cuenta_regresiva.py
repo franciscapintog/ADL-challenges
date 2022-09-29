1d52ac7a365b0a8d286a6a5104f4c401ff511d22
Francisca Pinto <86139385+franciscapintog@users.noreply.github.com>
2022-09-29 09:43:20 -0300

    Delete cuenta_regresiva.py

--- cuenta_regresiva.py
+++
@@ -1,8 +0,0 @@
-cr = int(input("Ingresa un numero para iniciar la cuenta regresiva:\n"))
-cr2 = cr + 1
-
-while cr > 0:
-        
-        if cr2 > 1:
-            cr2 -= 1
-            print(f"Iteracion {cr2}...")
